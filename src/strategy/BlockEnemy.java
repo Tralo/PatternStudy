3ce58c5ed9092b379520fb0bfe5c80bce3514497
Tralo <13580548169@163.com>
2016-09-01 10:27:42 +0800

    update

---
+++ src/strategy/BlockEnemy.java
@@ -0,0 +1,10 @@
+package strategy;
+
+public class BlockEnemy implements IStrategy{
+
+	public void operate() {
+		System.out.println("孙夫人断后,挡住追兵");
+		
+	}
+
+}
---
+++ src/strategy/GivenGreenLight.java
@@ -0,0 +1,10 @@
+package strategy;
+
+public class GivenGreenLight implements IStrategy{
+
+	public void operate() {
+		System.out.println("求吴国太开个绿灯,放行!");
+	}
+	
+
+}
